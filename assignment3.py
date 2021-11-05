0224f9aef2ba8efd330405c96697fd802b56424f
Rajat Singh <rajatsingh.b115@gmail.com>
2021-11-05 10:44:22 +0530

    bit counting

--- assignment3.py
+++ assignment3.py
@@ -85,6 +85,12 @@ def standardDeviation(counter):
 def randomness(D,C,N):
 	return (D*C)/N
 
+def count_ones(output1, output2):
+    cnt = 0 
+    for i in range(len(output1)):
+        if(output1[i]!=output2[i]):
+            cnt+=1
+    return cnt
 
 outputBytes = int(sys.argv[1])
 
@@ -102,6 +108,11 @@ flippingBitsArr = [i for i in range(1,33)]
 
 rArr=[0 for i in range(32)]
 
+xorOutputs_lis = []
+
+for i in range(32):
+    xorOutputs_lis.append([])
+
 for i in range(tot_itrations):
     for f in flippingBitsArr:
         flippedKey = flippingKeyBits(key, f)
@@ -111,11 +122,25 @@ for i in range(tot_itrations):
             print(len(output2))
         
         xorOutputs = xor(output1,output2)
+        xorOutputs_lis[f-1].append(xorOutputs)
+        
         counter = frequencyCountingTestForRandomnessTesting(xorOutputs)
         D = standardDeviation(counter)
         N = len(xorOutputs)
         C = len(counter)
         rArr[f-1] += (randomness(D,C,N))
+
+# checking the number of bits are similar
+for i in range(len(xorOutputs_lis)):
+    sm = 0
+    for j in range(1,len(xorOutputs_lis[0])):
+        #print(xorOutputs_lis[i][0])
+        #print(xorOutputs_lis[i][j])
+        sm+=count_ones(xorOutputs_lis[i][0],xorOutputs_lis[i][j])
+    print(sm/(len(xorOutputs_lis[0])-1))
+
+    
+    
 
 for i in range(32):
 	rArr[i]=rArr[i]/tot_itrations
